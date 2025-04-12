87857135bb085eb759f7ee494a4a119bce1e8b32
Matt Gibson <matthewjsgibson+github@gmail.com>
2025-04-12 21:26:32 +0000

    functional blog with front matter content

--- app/articles/[post]/page.tsx
+++ app/articles/[post]/page.tsx
@@ -1,37 +1,39 @@
-
 export default async function Page({
-    params,
-  }: {
-    params: Promise<{ post: string }>
-  }) {
-    const { post } = await params
-    console.log(post)
-    const mod = await import(`content/${post}.mdx`)
-    const { default: Post, meta } = mod
-    return (
+  params,
+}: {
+  params: Promise<{ post: string }>
+}) {
+
+  const { post } = await params
+  console.log(post)
+  const mod = await import(`content/${post}.mdx`)
+  const { default: Post, meta } = mod
+  return (
     <div>
-    <p className="text-sm text-neutral-900 tracking-tight">{meta.date}</p>
 
-    {meta.tags.map((tag) => {
-      return (
-        <span className="tracking-tight bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">{tag}</span>
-      )
+      <h1>{meta.title}</h1>
+      <p className="text-sm text-neutral-900 tracking-tight">{meta.date}</p>
 
-    })
+      {meta.tags.map((tag) => {
+        return (
+          <span className="tracking-tight bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">{tag}</span>
+        )
+
+      })
 
 
-  }
+      }
 
 
       <Post />
     </div>
 
-    )
-  }
-   
-  export function generateStaticParams() {
-    return [{ slug: 'first-post' }]
-  }
+  )
+}
 
-   
-  export const dynamicParams = false
+export function generateStaticParams() {
+  return [{ slug: 'first-post' }]
+}
+
+
+export const dynamicParams = false
