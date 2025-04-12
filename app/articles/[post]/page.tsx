a32dc6eef533a71cb84c6dc6ed883d775c9f7e24
Matt Gibson <matthewjsgibson+github@gmail.com>
2025-04-12 21:42:16 +0000

    build

--- app/articles/[post]/page.tsx
+++ app/articles/[post]/page.tsx
@@ -1,39 +1,29 @@
 export default async function Page({
   params,
 }: {
-  params: Promise<{ post: string }>
+  params: Promise<{ post: string }>;
 }) {
-
-  const { post } = await params
-  console.log(post)
-  const mod = await import(`content/${post}.mdx`)
-  const { default: Post, meta } = mod
+  const { post } = await params;
+  console.log(post);
+  const mod = await import(`content/${post}.mdx`);
+  const { default: Post, meta } = mod;
   return (
     <div>
-
       <h1>{meta.title}</h1>
       <p className="text-sm text-neutral-900 tracking-tight">{meta.date}</p>
 
       {meta.tags.map((tag) => {
         return (
-          <span className="tracking-tight bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">{tag}</span>
-        )
-
-      })
-
-
-      }
-
+          <span className="tracking-tight bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">
+            {tag}
+          </span>
+        );
+      })}
 
       <Post />
     </div>
-
-  )
-}
-
-export function generateStaticParams() {
-  return [{ slug: 'first-post' }]
+  );
 }
 
 
-export const dynamicParams = false
+export const dynamicParams = false;
--- app/components/blogitems.tsx
+++ app/components/blogitems.tsx
@@ -1,100 +1,83 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 import Link from "next/link";
 
 type BlogItem = {
-title?: string;
-date?: any;
-description?: string;
-tags?: string[];
-slug?: string;
+  title?: string;
+  date?: any;
+  description?: string;
+  tags?: string[];
+  slug?: string;
 };
 
-
-async function GetPosts(){
-    
-}
+async function GetPosts() {}
 
 export function getMdxSlugs(dirPath: string): string[] {
-return fs
+  return fs
     .readdirSync(dirPath)
-    .filter((file) => file.endsWith('.mdx'))
-    .map((file) => path.basename(file, '.mdx'))
+    .filter((file) => file.endsWith(".mdx"))
+    .map((file) => path.basename(file, ".mdx"));
 }
 
 export default async function BlogItems() {
-
-
-function compare(a: BlogItem, b: BlogItem) {
-    let a_date = new Date(a.date).getTime()
-    let b_date = new Date(b.date).getTime()
+  function compare(a: BlogItem, b: BlogItem) {
+    let a_date = new Date(a.date).getTime();
+    let b_date = new Date(b.date).getTime();
 
     if (a_date > b_date) {
-    return -1;
+      return -1;
     }
     if (a_date < b_date) {
-    return 1;
+      return 1;
     }
     return 0;
-}
+  }
 
+  const slugs = getMdxSlugs("content");
+  const objects: BlogItem[] = [];
 
+  for (let i = 0; i < slugs.length; i++) {
+    let obj: BlogItem = {};
 
+    const mod = await import(`content/${slugs[i]}.mdx`);
+    const { default: Post, meta } = mod;
+    obj.title = meta.title;
+    obj.description = meta.description;
+    obj.date = meta.date;
+    obj.tags = meta.tags;
+    obj.slug = slugs[i];
+    objects.push(obj);
+  }
 
-const slugs = getMdxSlugs("content")
-const objects: BlogItem[] = [];
+  objects.sort(compare);
 
-for (let i=0; i<slugs.length; i++){
-    let obj: BlogItem = {}
-
-    const mod = await import(`content/${slugs[i]}.mdx`)
-    const { default: Post, meta } = mod
-    obj.title = meta.title
-    obj.description = meta.description
-    obj.date = meta.date
-    obj.tags = meta.tags
-    obj.slug = slugs[i]
-    objects.push(obj)
-}
-
-
-objects.sort(compare);
-
-objects.forEach((story: BlogItem) => {
+  objects.forEach((story: BlogItem) => {
     const this_date_string: string = new Date(story.date).toLocaleDateString(
       "en-US",
     );
     story.date = this_date_string;
   });
 
-return (
+  return (
     <div>
-        {
-        objects.map((post) => {
-            return (
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                <Link 
-                key={"articles/" + post.slug}
-                href={"articles/" + post.slug}
-                className="transition-all flex align-middle relative">
-                <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums pb-2">
-                    {post.date}
-                </p>
-                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight hover:text-sky-300">
-                    {post.title}
-                </p>
-
-                </Link>
-                
-
-            </div>
-            )
-        }
-        
-        
-        )
-
-        }
+      {objects.map((post) => {
+        return (
+          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <Link
+              key={"articles/" + post.slug}
+              href={"articles/" + post.slug}
+              className="transition-all flex align-middle relative"
+            >
+              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums pb-2">
+                {post.date}
+              </p>
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight hover:text-sky-300">
+                {post.title}
+              </p>
+            </Link>
+          </div>
+        );
+      })}
     </div>
-);
+  );
 }
